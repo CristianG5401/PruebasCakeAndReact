7a1510e0bd745a289dd5553075184f0c15feeb93
CristianG5401 <cristian.vale540@pm.me>
2025-11-16 23:06:41 -0500

    🧹 CLEAN: remove redundant instructional text from App component
    - Removed "Click on the Vite and React logos to learn more" paragraph from default App template

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -13,9 +13,6 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
